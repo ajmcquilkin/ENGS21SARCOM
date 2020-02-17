1a76f196b5efe0261b3706115e8e6ab153d90a56
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-02-17 11:18:09 -0500

    Added batteryController class

---
+++ src/batteryController/BatteryController.cpp
@@ -0,0 +1,2 @@
+#include "BatteryController.h"
+using namespace std;
---
+++ src/batteryController/BatteryController.h
@@ -0,0 +1,9 @@
+// #include "../sensor/Sensor.h"
+#include <Sensor.h>
+
+class BatteryControler : public Sensor {
+  public:
+    BatteryControler() : Sensor() {
+
+    }
+};
