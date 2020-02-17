5b18bb155b2619e161ec4383b54946278bce6fb7
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-02-17 18:23:35 -0500

    Added RGB LED demo

--- src/core/core.ino
+++ src/core/core.ino
@@ -23,15 +23,25 @@
 
 #define VOLTAGE_ONE_PIN A0
 #define VOLTAGE_TWO_PIN A1
+#define VOLTAGE_THREE_PIN A2
+
+#define LED_PIN_R 3
+#define LED_PIN_G 5
+#define LED_PIN_B 6
 
 #define VOLTAGE_THRESHOLD 2
-int voltageOne, voltageTwo, pastVoltageOne, pastVoltageTwo;
+int voltageOne, voltageTwo, voltageThree, pastVoltageOne, pastVoltageTwo, pastVoltageThree;
 
 void setup() {
   Serial.begin(9600);
 
   pinMode(VOLTAGE_ONE_PIN, INPUT);
   pinMode(VOLTAGE_TWO_PIN, INPUT);
+  pinMode(VOLTAGE_THREE_PIN, INPUT);
+
+  pinMode(LED_PIN_R, OUTPUT);
+  pinMode(LED_PIN_G, OUTPUT);
+  pinMode(LED_PIN_B, OUTPUT);
 
   ClimateSensor clim = ClimateSensor(4);
   clim.init();
@@ -49,25 +59,37 @@ void setup() {
 
   pastVoltageOne = analogRead(VOLTAGE_ONE_PIN);
   pastVoltageTwo = analogRead(VOLTAGE_TWO_PIN);
+  pastVoltageThree = analogRead(VOLTAGE_THREE_PIN);
 }
 
 void loop() {
   voltageOne = analogRead(VOLTAGE_ONE_PIN);
   voltageTwo = analogRead(VOLTAGE_TWO_PIN);
+  voltageThree = analogRead(VOLTAGE_THREE_PIN);
+
+  analogWrite(LED_PIN_R, map(voltageOne, 1023, 0, 0, 255));
+  analogWrite(LED_PIN_G, map(voltageTwo, 1023, 0, 0, 255));
+  analogWrite(LED_PIN_B, map(voltageThree, 1023, 0, 0, 255));
 
   bool voltageOneDisplay = abs(voltageOne - pastVoltageOne) > VOLTAGE_THRESHOLD;
   bool voltageTwoDisplay = abs(voltageTwo - pastVoltageTwo) > VOLTAGE_THRESHOLD;
+  bool voltageThreeDisplay = abs(voltageThree - pastVoltageThree) > VOLTAGE_THRESHOLD;
 
-  if (voltageOneDisplay || voltageTwoDisplay) {
+  if (voltageOneDisplay || voltageTwoDisplay || voltageThreeDisplay) {
     Serial.print("Voltage One: ");
     Serial.print(voltageOne);
     Serial.print("\t");
     Serial.print("Voltage Two: ");
-    Serial.println(voltageTwo);
+    Serial.print(voltageTwo);
+    Serial.print("\t");
+    Serial.print("Voltage Three: ");
+    Serial.print(voltageThree);
+    Serial.println();
   }
 
   pastVoltageOne = voltageOne;
   pastVoltageTwo = voltageTwo;
+  pastVoltageThree = voltageThree;
 
-  delay(50);
+  delay(1);
 }
