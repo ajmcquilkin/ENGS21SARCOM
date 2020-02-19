da8b6cc2cd421553aec1ecc76fa5131943084ba9
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-02-18 21:24:25 -0500

    Implemented BatteryController

--- src/batteryController/BatteryController.cpp
+++ src/batteryController/BatteryController.cpp
@@ -2,3 +2,34 @@
 #include "BatteryController.h"
 using namespace std;
 
+void BatteryController::init() {
+  pinMode(_pin, INPUT);
+}
+
+float BatteryController::getBatteryVoltage() {
+  _checkBatteryVoltage();
+  return _voltage;
+}
+
+bool BatteryController::batteryWithinRange() {
+  return _checkBatteryVoltage();
+}
+
+float BatteryController::getMinBatteryVoltage() {
+  return _minSafeVoltage;
+}
+
+float BatteryController::getMaxBatteryVoltage() {
+  return _maxSafeVoltage;
+}
+
+bool BatteryController::_checkBatteryVoltage() {
+  _readBatteryVoltage();
+  if (_voltage < _minSafeVoltage || _maxSafeVoltage < _voltage) {
+    return false;
+  } return true;
+}
+
+float BatteryController::_readBatteryVoltage() {
+  _voltage = analogRead(_pin) * (_voltageReference / _referenceResolution);
+}
--- src/batteryController/BatteryController.h
+++ src/batteryController/BatteryController.h
@@ -1,8 +1,51 @@
+#include <Arduino.h>
 #include <Sensor.h>
 
-class BatteryControler : public Sensor {
+class BatteryController : public Sensor {
   public:
-    BatteryControler() : Sensor() {
+    enum BatteryType { LiPo, NiMH };
+    BatteryController(int pin, BatteryType b, float voltageReference=5.00, int referenceResolution=1024) : Sensor() {
+      _pin = pin;
+      _voltageReference = voltageReference;
+      _referenceResolution = referenceResolution;
+      _batteryType = b;
 
+      switch (_batteryType) {
+        case BatteryType::LiPo:
+          _minSafeVoltage = 3.7;
+          _maxSafeVoltage = 4.2;
+          break;
+
+        case BatteryType::NiMH:
+          _minSafeVoltage = 1.0;
+          _maxSafeVoltage = 1.5;
+          break;
+
+        default:
+          _minSafeVoltage = -1.0;
+          _maxSafeVoltage = -1.0;
+          break;
+      }
     }
+
+    void init();
+    float getBatteryVoltage();
+    bool batteryWithinRange();
+
+    float getMinBatteryVoltage();
+    float getMaxBatteryVoltage();
+
+  private:
+    int _pin;
+    BatteryType _batteryType;
+
+    float _voltage;
+    float _voltageReference;
+    int _referenceResolution;
+
+    float _minSafeVoltage;
+    float _maxSafeVoltage;
+
+    bool _checkBatteryVoltage();
+    float _readBatteryVoltage();
 };
